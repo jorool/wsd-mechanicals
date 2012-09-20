ceaabf3c381736ddc65ba68da345d6c439af5a6c
Jonathan Rodrigues de Oliveira <jorool1988@gmail.com>
2012-09-19 21:03:37 -0300

    Criando projeto maven

---
+++ src/main/java/br/uem/din/jroliveira/wsd_mechanicals/App.java
@@ -0,0 +1,13 @@
+package br.uem.din.jroliveira.wsd_mechanicals;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
