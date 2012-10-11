ee55abcfa911ac559b59af950d4506dada372341
Jonathan Rodrigues de Oliveira <jorool1988@gmail.com>
2012-10-11 17:59:59 -0300

    Corrigindo configuração.

--- src/main/java/org/wsd/mechanicals/config/root/RepositoryConfig.java
+++ src/main/java/org/wsd/mechanicals/config/root/RepositoryConfig.java
@@ -10,13 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.wsd.mechanicals.app.contato.Contato;
-import org.wsd.mechanicals.app.notafiscal.NotaFiscal;
-import org.wsd.mechanicals.app.pedido.Pedido;
-import org.wsd.mechanicals.app.produto.Produto;
-import org.wsd.mechanicals.app.seguranca.Usuario;
 
-@EnableJpaRepositories(basePackageClasses = {Usuario.class, Produto.class, Pedido.class, NotaFiscal.class, Contato.class})
+@EnableJpaRepositories(basePackages = "org.wsd.mechanicals.app")
 @EnableTransactionManagement(proxyTargetClass = true)
 @Configuration
 @ImportResource("classpath*:META-INF/spring/applicationContext-data.xml")
--- src/main/java/org/wsd/mechanicals/config/webflow/WebflowServletConfig.java
+++ src/main/java/org/wsd/mechanicals/config/webflow/WebflowServletConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "org.wsd.mechanicals.app", scopedProxy = ScopedProxyMode.TARGET_CLASS, useDefaultFilters = false, includeFilters = {
+@ComponentScan(basePackages = {"org.wsd.mechanicals.app", "org.wsd.mechanicals.config.root"}, scopedProxy = ScopedProxyMode.TARGET_CLASS, useDefaultFilters = false, includeFilters = {
 		@Filter(Controller.class), @Filter(Configuration.class) })
 @ImportResource("classpath*:META-INF/spring/servlet/webflow-servlet.xml")
 public class WebflowServletConfig extends WebMvcConfigurerAdapter {
